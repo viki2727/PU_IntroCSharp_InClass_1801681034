2c7e00ee8d0898d600720b8a31617afcec847f1e
viki2727 <viki2727@abv.bg>
2018-10-16 15:51:24 +0300

    commit musicfactory

--- W3InClass/MusicFactory/Program.cs
+++ W3InClass/MusicFactory/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,32 @@ namespace MusicFactory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Play some music...");
+            Console.WriteLine("Do you want to play some music?");
+          string answer = Console.ReadLine();
+
+            if (answer=="Yes")
+            {
+                using (SoundPlayer player = new SoundPlayer(@"C:\Users\fmi\Desktop\w3\PU_IntroCSharp_InClass_1801681034\W3InClass\MusicFactory\music\Dynoro feat. Gigi DAgostino - In My Mind.mp3"))
+                { 
+                    
+                    player.PlaySync();
+                    while(true)
+                    {
+                        Console.WriteLine(".");
+                    }
+                }
+            }      
+            else if (answer== "No")
+            {
+                Console.WriteLine("GoodBye");
+            }
+               else
+            {
+                Console.WriteLine("Wrong input");
+            }
+        
+           
+
         }
     }
 }
